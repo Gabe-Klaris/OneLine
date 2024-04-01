9a9849539b2836632286cd40a7cb8dc2f6469c3b
kirbyrocks64 <159724135+kirbyrocks64@users.noreply.github.com>
2024-03-31 22:48:02 -0400

    added main menu, credits menu, pause menu
    - pause menu has full functionality
    - credits + quit works on main menu
    - added game handler and pause menu handler
    - need main menu music & level music to finish audio manager object

---
+++ OneLine/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameHandler : MonoBehaviour
+{
+    private GameObject player;
+    private string sceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneName = SceneManager.GetActiveScene().name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        // SceneManager.LoadScene("Level1");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void Options()
+    {
+
+    }
+
+    public void ResetLevel()
+    {
+        Time.timeScale = 1f;
+        GameHandler_PauseMenu.GameisPaused = false;
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        GameHandler_PauseMenu.GameisPaused = false;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+
+    public void CreditsBack()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
